f94f43f9b6b2aad3df95c9cdfd94f2399dd06080
Erfan Saraiy <48406371+Erfan-react-80@users.noreply.github.com>
2023-03-02 18:29:20 +0330

    Create seglib.h

---
+++ seglib.h
@@ -0,0 +1,30 @@
+// Necessary files on main.h
+#include "main.h"
+
+/* 7Segment Type (CC or CA) */
+//#define SEGCC
+#define SEGCA
+
+/* Common Pins */
+#define CC1 10
+#define CC2 11
+#define CC3 12
+#define CC4 13
+
+/* Individual Pins */
+#define DA 0
+#define DB 1
+#define DC 2
+#define DD 3
+#define DE 4
+#define DF 5
+#define DG 6
+
+/* Quickly group pin define
+#define ALLDX (1 << DA) | (1 << DB) | (1 << DC) | (1 << DD) | (1 << DE) | (1 << DF) | (1 << DG) | (1 << CC1)
+#define ALLCCX (1 << CC1) | (1 << CC2) | (1 << CC3) | (1 << CC4)
+
+void segConfig(void);
+void segWriteDigit(unsigned char digit, unsigned char pos);
+void segClear(void);
+void segWrite(int num);
